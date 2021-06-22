56824aaf24b315a02e952011ddfa60dddf17e39d
Allanlimo02 <allan.limo@student.moringaschool.com>
2021-06-22 10:21:29 +0300

    Fixed subcribe button in landing page

--- scripter.js
+++ scripter.js
@@ -1,59 +1,13 @@
-// function fvalid() {
-//     var name = document.forms["RegForm"]["Name"];
-//     var email = document.forms["RegForm"]["EMail"];
-//     var phone = document.forms["RegForm"]["Telephone"];
-//     var what = document.forms["RegForm"]["Subject"];
-//     var password = document.forms["RegForm"]["Password"];
-//     var address = document.forms["RegForm"]["Address"];
-   
-
-//     if (name.value == "") {
-//         alert("Dear User, Please enter your name.");
-//         name.focus();
-//         return false;
-//     }
-
-//     if (address.value == "") {
-//         window.alert("Dear "+name.value+" Please enter your address.");
-//         address.focus();
-//         return false;
-//     }
-
-//     if (email.value == "") {
-//         window.alert(
-//             "Dear "+name.value+" Please enter a valid e-mail address.");
-//         email.focus();
-//         return false;
-//     }
-
-//     if (phone.value == "") {
-//         window.alert(
-//             "Dear "+name.value+" Please enter your telephone number.");
-//         phone.focus();
-//         return false;
-//     }
-
-//     if (password.value == "") {
-//         window.alert("Dear "+name.value+" Please enter your password");
-//         password.focus();
-//         return false;
-//     }
-
-//     if (what.selectedIndex < 1) {
-//         alert("Dear "+name.value+" Please delect type of service.");
-//         what.focus();
-//         return false;
-//     }
-
-//     window.alert("Thank you "+name.value+" For your submission")
-// }
 
 function validater(){
     var fName=document.getElementById('fname');
     var sName= document.getElementById('lname');
     var mail = document.getElementById('email');
     var phone= document.getElementById('phone');
-    if(fName.value==''){
+
+// if (fName.value == '' || sName)
+
+    if(fName.value=='' ){
         alert('Dear user, input your first name');
         fName.focus();
         return false;
@@ -76,5 +30,5 @@ function validater(){
     
 
     alert('Thank you '+fName.value+'For submitting this form')
-    return true;
-}
+    // return true;
+};
